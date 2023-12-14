dff2d3abca2e9989bae24e8a716fa567897d2cf7
Pablo <murapabytes@gmail.com>
2023-12-15 00:37:24 +0100

    fix: error on base repo

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
 	 *
 	 * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
 	 */
-	basePath: "/murapa96.github.io",
+	basePath: "/murapa96/murapa96.github.io",
   
 	/**
 	 * Disable server-based image optimization. Next.js does not support
